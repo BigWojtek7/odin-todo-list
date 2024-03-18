8c58c23f02b8d318e25ba264a71fa76f4abec1d3
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-03-18 20:06:01 +0100

    Add functionality do change project

--- src/DOM.js
+++ src/DOM.js
@@ -28,7 +28,7 @@ function displayHandler(){
 
     let newDivProject = document.createElement("div");
     newDivProject.dataset.project = todoArray[0].project
-    newDivProject.innerHTML = `${todoArray[0].project} <button>Expand</button>`
+    newDivProject.innerHTML = `<p class="title">${todoArray[0].project}</p> <button>Expand</button>`
 
     todoArray.forEach((el, index) => {
       const newDiv = document.createElement("div");
@@ -37,14 +37,15 @@ function displayHandler(){
       // newDivProject.appendChild(newButtonExpand);
       console.log(el)
 
-      newDiv.innerHTML = `${el.title}, ${el.description}, ${el.dueDate} <button data-index=${index}>Delete</button>`;
+      newDiv.innerHTML = `${el.title}, ${el.description}, ${el.dueDate} 
+      <button data-index=${index}>Delete</button> <button data-expand=${index}>Project</button>`;
       
       if (newDivProject.dataset.project === el.project){
         newDivProject.appendChild(newDiv);
       } else {
         divContent.appendChild(newDivProject);
         newDivProject = document.createElement("div");
-        newDivProject.innerHTML = `${el.project} <button>Expand</button>`
+        newDivProject.innerHTML = `<p class="title">${el.project}</p> <button>Expand</button>`
         newDivProject.dataset.project = el.project;
         // newDivProject.appendChild(newButtonExpand);
         newDivProject.appendChild(newDiv);
@@ -59,26 +60,56 @@ function displayHandler(){
     const datasetButton = e.target.dataset.index;
     const datasetDiv = e.target.closest("div").dataset.project;
 
-    const dialogExpand = document.querySelector(".dialog-expand")
+    const expandButton = e.target.dataset.expand;
+
+    const dialogExpand = document.getElementById("dialog-expand");
 
     if(datasetButton){
       deleteObject(datasetButton);
       updateDisplay();
-    } else if(datasetDiv) {
-        const todoArray = loadObject().getArray();
-        dialogExpand.innerHTML= "";
-        // console.log(todoArray.filter(item => div.dataset.project === item.project));
-        console.log(datasetDiv)
-        const projectArray = todoArray.filter(item => datasetDiv === item.project)
-        console.log("funny", projectArray)
-        projectArray.forEach(el => {
-          
-          const newDiv = document.createElement("div")
-          newDiv.innerHTML=`${el.project}, ${el.title}, ${el.description}, ${el.dueDate}, ${el.priority}, ${el.isDone},`
-          dialogExpand.appendChild(newDiv);
-          dialogExpand.showModal();
-        });
+    } else if (expandButton){
+      const todoArray = loadObject().getArray();
       
+      const changeProjectDialog = document.getElementById("change-project-dialog");
+      const changeProjectForm = document.getElementById("change-project-form");
+      const closeProjectButton = document.getElementById("close-project-button");
+      changeProjectDialog.showModal();
+      changeProjectForm.addEventListener("submit", () =>{
+        const changeProjectInput = document.getElementById("change-project-input")
+
+        todoArray[expandButton].project = changeProjectInput.value
+        saveObject(todoArray);
+        updateDisplay();
+        changeProjectDialog.close();
+        form.reset();
+
+      })
+
+      closeProjectButton.addEventListener("click", () => changeProjectDialog.close());
+
+
+
+
+      
+
+    } else if(datasetDiv) {
+      const todoArray = loadObject().getArray();
+      dialogExpand.innerHTML= "";
+      // console.log(todoArray.filter(item => div.dataset.project === item.project));
+      console.log(datasetDiv)
+      const projectArray = todoArray.filter(item => datasetDiv === item.project)
+      console.log("funny", projectArray)
+      projectArray.forEach(el => {
+        
+        const newDiv = document.createElement("div")
+        newDiv.innerHTML=`${el.project}, ${el.title}, ${el.description}, ${el.dueDate}, ${el.priority}, ${el.isDone}`
+        dialogExpand.appendChild(newDiv);
+        dialogExpand.showModal();
+      });
+      const closeExpandButton = document.createElement("button")
+      closeExpandButton.innerHTML = "Close"
+      dialogExpand.appendChild(closeExpandButton);
+      closeExpandButton.addEventListener("click", () => dialogExpand.close());
 
     }
   }
